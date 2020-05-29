b9d9dc3f7865abf85ecb89f1327606bacd102895
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2020-05-29 15:44:46 +0300

    EP-2214

---
+++ src/components/EpToteutussuunnitelmaListaus/EpToteutussuunnitelmaListaus.vue
@@ -0,0 +1,325 @@
+<template>
+  <div>
+    <div class="header">
+      <slot name="header">
+        <h1 class="bg-danger">slot: header</h1>
+      </slot>
+    </div>
+
+    <div class="filters" v-if="items">
+      <div class="d-lg-flex align-items-end">
+        <div class="mt-2 mb-2 mr-2 flex-fill">
+          <EpSearch v-model="query.nimi" :placeholder="$t('etsi-toteutussuunnitelmaa-tai-jaettua-osaa')"/>
+        </div>
+        <div class="m-2 flex-fill" v-if="filtersInclude('tyyppi')">
+          <label>{{ $t('tyyppi') }}</label>
+          <EpMultiSelect v-model="tyyppi"
+                    :enable-empty-option="true"
+                    :placeholder="$t('kaikki')"
+                    :is-editing="true"
+                    :options="vaihtoehdotTyypit">
+            <template slot="singleLabel" slot-scope="{ option }">
+              {{ $t('amosaa-tyyppi-' + option) }}
+            </template>
+            <template slot="option" slot-scope="{ option }">
+              {{ $t('amosaa-tyyppi-' + option) }}
+            </template>
+          </EpMultiSelect>
+        </div>
+        <div class="m-2 flex-fill" v-if="filtersInclude('voimassaolo')">
+          <label>{{ $t('voimassaolo') }}</label>
+          <EpMultiSelect v-model="voimassaolo"
+                    :enable-empty-option="true"
+                    :placeholder="$t('kaikki')"
+                    :is-editing="true"
+                    :options="vaihtoehdotVoimassaolo">
+            <template slot="singleLabel" slot-scope="{ option }">
+              {{ $t('ajoitus-' + option.toLowerCase()) }}
+            </template>
+            <template slot="option" slot-scope="{ option }">
+              {{ $t('ajoitus-' + option.toLowerCase()) }}
+            </template>
+          </EpMultiSelect>
+        </div>
+        <div class="mb-3">
+          <EpSpinner v-if="isLoading" />
+        </div>
+      </div>
+
+      <div class="d-lg-flex align-items-end">
+        <div class="m-2" v-if="filtersInclude('tila')">
+          <b-form-checkbox-group v-model="tila">
+            <b-form-checkbox v-for="tila in vaihtoehdotTilat" :key="tila" :value="tila">
+              {{ $t('tila-' + tila.toLowerCase()) }}
+            </b-form-checkbox>
+          </b-form-checkbox-group>
+        </div>
+      </div>
+
+      <div v-if="items.data.length > 0">
+        <b-table striped hover responsive :items="items.data" :fields="fields">
+          <template v-slot:cell(nimi)="data">
+            <router-link :to="{}">
+              {{ $kaanna(data.item.nimi) }}
+            </router-link>
+          </template>
+        </b-table>
+        <ep-pagination v-model="sivu"
+                       :per-page="perPage"
+                       :total-rows="total"/>
+      </div>
+      <div v-else class="m-2 alert alert-info">
+        {{ $t('ei-hakutuloksia') }}
+      </div>
+    </div>
+    <EpSpinner v-else />
+  </div>
+</template>
+
+<script lang="ts">
+import { Watch, Prop, Component, Vue } from 'vue-property-decorator';
+import EpMainView from '@shared/components/EpMainView/EpMainView.vue';
+import EpIcon from '@shared/components/EpIcon/EpIcon.vue';
+import EpPagination from '@shared/components/EpPagination/EpPagination.vue';
+import EpSearch from '@shared/components/forms/EpSearch.vue';
+import EpMultiSelect from '@shared/components/forms/EpMultiSelect.vue';
+import EpSpinner from '@shared/components/EpSpinner/EpSpinner.vue';
+import { OpetussuunnitelmaDto } from '@shared/api/amosaa';
+import { Page } from '@shared/tyypit';
+import { BvTableFieldArray } from 'bootstrap-vue';
+import { IToteutussuunnitelmaProvider } from './types';
+import * as _ from 'lodash';
+
+export type ProjektiFilter = 'koulutustyyppi' | 'tila' | 'voimassaolo';
+
+@Component({
+  components: {
+    EpIcon,
+    EpMainView,
+    EpMultiSelect,
+    EpPagination,
+    EpSearch,
+    EpSpinner,
+  },
+})
+export default class EpToteutussuunnitelmaListaus extends Vue {
+  @Prop({ required: true })
+  provider!: IToteutussuunnitelmaProvider;
+
+  @Prop({ required: false })
+  fieldKeys!: string[];
+
+  @Prop({ required: false, default: () => ['tyyppi', 'voimassaolo', 'tila'] })
+  filters!: ProjektiFilter[];
+
+  private tyyppi: string | null = null;
+  private voimassaolo: string | null = null;
+  private tila: string[] | null = ['luonnos', 'julkaistu'];
+  private isLoading = false;
+
+  private query = {
+    sivu: 0,
+    sivukoko: 10,
+    voimassaolo: true,
+    siirtyma: true,
+    tuleva: true,
+    poistunut: true,
+    tila: this.tila,
+    tyyppi: ['pohja', 'ops', 'yleinen', 'yhteinen'],
+    nimi: '',
+    jarjestysOrder: false,
+    jarjestysTapa: 'nimi',
+  } as any;
+
+  async mounted() {
+    this.provider.updateQuery(this.query);
+  }
+
+  @Watch('query', { deep: true, immediate: true })
+  async onQueryChange(query: any) {
+    this.isLoading = true;
+    try {
+      await this.provider.updateQuery({
+        ...query,
+      });
+    }
+    finally {
+      this.isLoading = false;
+    }
+  }
+
+  @Watch('voimassaolo')
+  onChangeVoimassaolo(tila: string) {
+    const defaults = {
+      voimassaolo: false,
+      siirtyma: false,
+      tuleva: false,
+      poistunut: false,
+    };
+
+    switch (tila) {
+    case 'tuleva':
+      this.query = { ...defaults, tuleva: true };
+      break;
+    case 'voimassaolo':
+      this.query = { ...defaults, voimassaolo: true };
+      break;
+    case 'poistunut':
+      this.query = { ...defaults, poistunut: true };
+      break;
+    default:
+      this.query = {
+        ...defaults,
+        voimassaolo: true,
+        tuleva: true,
+        poistunut: true,
+      };
+      break;
+    }
+  }
+
+  @Watch('tila')
+  onTilaChange(tila: string) {
+    this.query = {
+      ...this.query,
+      tila: tila
+        ? [tila]
+        : ['luonnos', 'julkaistu', 'poistettu'],
+    };
+  }
+
+  @Watch('tyyppi')
+  onTyyppiChange(tyyppi: string) {
+    this.query.tyyppi = [tyyppi];
+  }
+
+  get vaihtoehdotTyypit() {
+    return ['pohja', 'ops', 'yleinen', 'yhteinen'];
+  }
+
+  get vaihtoehdotVoimassaolo() {
+    return [
+      'kaikki',
+      'tuleva',
+      'voimassaolo',
+      'poistunut',
+    ];
+  }
+
+  get vaihtoehdotTilat() {
+    return ['luonnos', 'julkaistu', 'poistettu'];
+  }
+
+  get sivu() {
+    return this.items?.sivu! + 1;
+  }
+
+  set sivu(value: number) {
+    this.query.sivu = value - 1;
+  }
+
+  get perPage() {
+    return this.items?.sivukoko || 10;
+  }
+
+  get total() {
+    return this.items?.kokonaismäärä || 0;
+  }
+
+  get items() {
+    return this.provider.opetussuunnitelmat.value;
+  }
+
+  get fields() {
+    return _.filter(this.initialFields, (field: any) => !this.fieldKeys || _.includes(this.fieldKeys, field.key));
+  }
+
+  get initialFields(): BvTableFieldArray {
+    const dateFormatter = (value: Date) => {
+      return value
+        ? this.$sd(value)
+        : '-';
+    };
+
+    return [{
+      key: 'nimi',
+      label: this.$t('nimi') as string,
+      sortable: true,
+    }, {
+      key: 'tyyppi',
+      sortable: true,
+      label: this.$t('tyyppi') as string,
+    }, {
+      key: 'tila',
+      sortable: true,
+      label: this.$t('tila') as string,
+      formatter: (value: any, key: string, item: OpetussuunnitelmaDto) => {
+        return this.$t('tila-' + item!.tila);
+      },
+    }, {
+      key: 'koulutuskoodi',
+      sortable: true,
+      label: this.$t('koulutuskoodi') as string,
+      formatter: (value: any, key: string, item: OpetussuunnitelmaDto) => {
+        if (item.peruste && item.peruste.koulutukset) {
+          return _.head(item!.peruste!.koulutukset)!['koulutuskoodiArvo'];
+        }
+      },
+    }, {
+      key: 'luotu',
+      sortable: true,
+      label: this.$t('luotu') as string,
+      formatter: dateFormatter,
+    }, {
+      key: 'muokattu',
+      sortable: true,
+      label: this.$t('muokattu') as string,
+      formatter: dateFormatter,
+    }, {
+      key: 'voimaantulo',
+      sortable: true,
+      label: this.$t('voimassaolo-alkaa') as string,
+      formatter: dateFormatter,
+    }, {
+      key: 'paatospaivamaara',
+      sortable: true,
+      label: this.$t('voimassaolo-loppuu') as string,
+      formatter: dateFormatter,
+    }];
+  }
+
+  filtersInclude(filter: any) {
+    return !this.filters || _.includes(this.filters, filter);
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.upper {
+  margin-bottom: 3rem;
+}
+
+.lower {
+  margin-top: 4rem;
+}
+
+.teksti {
+  font-size: 22px;
+  font-weight: 400;
+}
+
+.ikoni {
+  font-size: 38px;
+  font-weight: 100;
+}
+
+.card-wrapper {
+  margin: 0 20px 20px 0;
+}
+
+.small-text {
+  color: #1d346b;
+}
+
+</style>
---
+++ src/views/RouteToteutussuunnitelmat.vue
@@ -0,0 +1,45 @@
+<template>
+  <EpMainView>
+    <EpToteutussuunnitelmaListaus :provider="toteutussuunnitelmatStore">
+      <div slot="header" class="d-flex justify-content-between">
+        <h2>{{ $t('toteutussuunnitelmat') }}</h2>
+
+        <div>
+          <router-link :to="{}">
+            <ep-button variant="outline-primary" icon="plussa">
+              {{ $t('lisaa-toteutussuunnitelma') }}
+            </ep-button>
+          </router-link>
+
+          <router-link :to="{}">
+            <ep-button variant="outline-primary" icon="plussa" >
+              {{ $t('lisaa-jaettu-osa') }}
+            </ep-button>
+          </router-link>
+        </div>
+      </div>
+    </EpToteutussuunnitelmaListaus>
+  </EpMainView>
+</template>
+
+<script lang="ts">
+import { Prop, Component, Vue } from 'vue-property-decorator';
+import EpMainView from '@shared/components/EpMainView/EpMainView.vue';
+import EpToteutussuunnitelmaListaus from '@/components/EpToteutussuunnitelmaListaus/EpToteutussuunnitelmaListaus.vue';
+import EpIcon from '@shared/components/EpIcon/EpIcon.vue';
+import { ToteutussuunnitelmatStore } from '@/store/ToteutussuunnitelmatStore';
+import EpButton from '@shared/components/EpButton/EpButton.vue';
+
+@Component({
+  components: {
+    EpIcon,
+    EpMainView,
+    EpToteutussuunnitelmaListaus,
+    EpButton,
+  },
+})
+export default class RoutePerusteprojektit extends Vue {
+  @Prop({ required: true })
+  toteutussuunnitelmatStore!: ToteutussuunnitelmatStore;
+}
+</script>
