5c7eae7f2893274b79bd337d7cf1fd454d683f4c
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2020-06-05 14:07:55 +0300

    EP-2233

--- src/stores/ToteutussuunnitelmaStore.ts
+++ src/stores/ToteutussuunnitelmaStore.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import VueCompositionApi, { reactive, computed } from '@vue/composition-api';
-import { OpetussuunnitelmaDto, Opetussuunnitelmat, NavigationNodeDto } from '@shared/api/amosaa';
+import { OpetussuunnitelmaDto, Opetussuunnitelmat, NavigationNodeDto, OpetussuunnitelmaLuontiDto } from '@shared/api/amosaa';
 import _ from 'lodash';
 
 Vue.use(VueCompositionApi);
@@ -15,7 +15,12 @@ export class ToteutussuunnitelmaStore {
   public readonly navigation = computed(() => this.state.navigation);
 
   public async init(koulutustoimijaId: number, toteutussuunnitelmaId: number) {
+    this.state.toteutussuunnitelma = null;
     this.state.toteutussuunnitelma = (await Opetussuunnitelmat.getOpetussuunnitelma(toteutussuunnitelmaId, _.toString(koulutustoimijaId))).data;
     this.state.navigation = (await Opetussuunnitelmat.getOpetussuunnitelmaNavigation(toteutussuunnitelmaId, _.toString(koulutustoimijaId))).data;
+  }
+
+  public async create(ktId: string, toteutussuunnitelma: OpetussuunnitelmaLuontiDto) {
+    return (await Opetussuunnitelmat.addOpetussuunnitelma(ktId, toteutussuunnitelma)).data;
   }
 }
