f30064aab587630ead551e9a1a7da458254ca335
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2020-07-15 09:54:21 +0300

    dump

--- src/stores/TekstikappaleStore.ts
+++ src/stores/TekstikappaleStore.ts
@@ -9,12 +9,12 @@ Vue.use(VueCompositionApi);
 
 export class TekstikappaleStore implements IEditoitava {
   constructor(
-    private opetussuunnitelmaId: number,
-    private koulutustoimijaId: string,
-    private sisaltoviiteId: number,
-    private versionumero: number,
-    private el: any,
-    private updateNavigation: Function,
+    private opetussuunnitelmaId?: number,
+    private koulutustoimijaId?: string,
+    private sisaltoviiteId?: number,
+    private versionumero?: number,
+    private el?: any,
+    private updateNavigation?: Function,
   ) {
   }
 
@@ -30,26 +30,26 @@ export class TekstikappaleStore implements IEditoitava {
 
   async load() {
     if (this.versionumero) {
-      const revisions = (await Sisaltoviitteet.getSisaltoviiteRevisions(this.opetussuunnitelmaId, this.sisaltoviiteId, this.koulutustoimijaId)).data as Revision[];
+      const revisions = (await Sisaltoviitteet.getSisaltoviiteRevisions(this.opetussuunnitelmaId!, this.sisaltoviiteId!, this.koulutustoimijaId!)).data as Revision[];
       const rev = revisions[revisions.length - this.versionumero];
-      return (await Sisaltoviitteet.getSisaltoviiteRevision(this.opetussuunnitelmaId, this.sisaltoviiteId, rev.numero, this.koulutustoimijaId)).data;
+      return (await Sisaltoviitteet.getSisaltoviiteRevision(this.opetussuunnitelmaId!, this.sisaltoviiteId!, rev.numero, this.koulutustoimijaId!)).data;
     }
     else {
-      return (await Sisaltoviitteet.getSisaltoviiteTekstit(this.opetussuunnitelmaId, this.sisaltoviiteId, this.koulutustoimijaId)).data;
+      return (await Sisaltoviitteet.getSisaltoviiteTekstit(this.opetussuunnitelmaId!, this.sisaltoviiteId!, this.koulutustoimijaId!)).data;
     }
   }
 
   async save(data: any) {
-    await Sisaltoviitteet.updateTekstiKappaleViite(this.opetussuunnitelmaId, this.sisaltoviiteId, this.koulutustoimijaId, data);
+    await Sisaltoviitteet.updateTekstiKappaleViite(this.opetussuunnitelmaId!, this.sisaltoviiteId!, this.koulutustoimijaId!, data);
   }
 
   async restore(rev: number) {
-    const restoring = (await Sisaltoviitteet.getSisaltoviiteRevision(this.opetussuunnitelmaId, this.sisaltoviiteId, rev, this.koulutustoimijaId)).data;
-    await Sisaltoviitteet.updateTekstiKappaleViite(this.opetussuunnitelmaId, this.sisaltoviiteId, this.koulutustoimijaId, restoring);
+    const restoring = (await Sisaltoviitteet.getSisaltoviiteRevision(this.opetussuunnitelmaId!, this.sisaltoviiteId!, rev, this.koulutustoimijaId!)).data;
+    await Sisaltoviitteet.updateTekstiKappaleViite(this.opetussuunnitelmaId!, this.sisaltoviiteId!, this.koulutustoimijaId!, restoring);
   }
 
   async revisions() {
-    const data = (await Sisaltoviitteet.getSisaltoviiteRevisions(this.opetussuunnitelmaId, this.sisaltoviiteId, this.koulutustoimijaId)).data;
+    const data = (await Sisaltoviitteet.getSisaltoviiteRevisions(this.opetussuunnitelmaId!, this.sisaltoviiteId!, this.koulutustoimijaId!)).data;
     return data as Revision[];
   }
 
@@ -57,7 +57,7 @@ export class TekstikappaleStore implements IEditoitava {
   }
 
   async remove() {
-    await Sisaltoviitteet.removeSisaltoViite(this.opetussuunnitelmaId, this.sisaltoviiteId, this.koulutustoimijaId);
+    await Sisaltoviitteet.removeSisaltoViite(this.opetussuunnitelmaId!, this.sisaltoviiteId!, this.koulutustoimijaId!);
     await this.updateNavigation();
     this.el.$router.replace({
       name: 'toteutussuunnitelma',
@@ -69,7 +69,7 @@ export class TekstikappaleStore implements IEditoitava {
 
   public async lock() {
     try {
-      const res = await SisaltoviiteLukko.getLock(_.toNumber(this.koulutustoimijaId), this.opetussuunnitelmaId, this.sisaltoviiteId);
+      const res = await SisaltoviiteLukko.getLock(_.toNumber(this.koulutustoimijaId), this.opetussuunnitelmaId!, this.sisaltoviiteId!);
       return res.data as ILukko;
     }
     catch (err) {
@@ -78,12 +78,12 @@ export class TekstikappaleStore implements IEditoitava {
   }
 
   public async acquire() {
-    const res = await SisaltoviiteLukko.lock(_.toNumber(this.koulutustoimijaId), this.opetussuunnitelmaId, this.sisaltoviiteId);
+    const res = await SisaltoviiteLukko.lock(_.toNumber(this.koulutustoimijaId), this.opetussuunnitelmaId!, this.sisaltoviiteId!);
     return res.data as ILukko;
   }
 
   public async release() {
-    await SisaltoviiteLukko.unlock(_.toNumber(this.koulutustoimijaId), this.opetussuunnitelmaId, this.sisaltoviiteId);
+    await SisaltoviiteLukko.unlock(_.toNumber(this.koulutustoimijaId), this.opetussuunnitelmaId!, this.sisaltoviiteId!);
   }
 
   public features(data: any) {
