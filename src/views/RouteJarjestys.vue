a379b2ded3ce799be92a5a7facee5ed0a1a555a5
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2020-08-14 10:50:38 +0300

    EP-2254

--- src/views/RouteJarjestys.vue
+++ src/views/RouteJarjestys.vue
@@ -1,16 +1,72 @@
 <template>
-<div >
-RouteJarjestys
-</div>
+  <div id="scroll-anchor" v-if="editointiStore" >
+    <EpEditointi :store="editointiStore">
+      <template v-slot:header="{ data }">
+        <h2 class="m-0">{{ $t('muokkaa-jarjestysta') }}</h2>
+      </template>
+
+      <template v-slot:default="{ data, isEditing }">
+
+        <div v-for="(rakenne, index) in data.rakenteet" :key="'rakenne'+index" class="mb-5">
+          <h3>{{$t(rakenne.otsikko)}}</h3>
+
+          <ep-jarjesta v-if="rakenne.sisalto && rakenne.sisalto.length > 0"
+              :isEditable="isEditing"
+              v-model="rakenne.sisalto"
+              :group="rakenne.otsikko"
+              :child-field="rakenne.lapset">
+            <template #default="{ node }">
+              <span>
+                {{ $kaanna(node.tekstiKappale.nimi) }}
+              </span>
+            </template>
+          </ep-jarjesta>
+
+          <div v-else class="font-italic">{{$t('rakenne-puuttuu-' + rakenne.otsikko)}}</div>
+
+        </div>
+
+      </template>
+
+    </EpEditointi>
+  </div>
 </template>
 
 <script lang="ts">
 import _ from 'lodash';
 import { Prop, Mixins, Component, Vue, Watch } from 'vue-property-decorator';
+import { EditointiStore } from '@shared/components/EpEditointi/EditointiStore';
+import { RakenneStore } from '@/stores/RakenneStore';
+import EpEditointi from '@shared/components/EpEditointi/EpEditointi.vue';
+import EpJarjesta from '@shared/components/EpJarjesta/EpJarjesta.vue';
+import { ToteutussuunnitelmaStore } from '@/stores/ToteutussuunnitelmaStore';
 
-@Component
+@Component({
+  components: {
+    EpEditointi,
+    EpJarjesta,
+  },
+})
 export default class RouteJarjestys extends Vue {
+  @Prop({ required: true })
+  private koulutustoimijaId!: string;
 
+  @Prop({ required: true })
+  private toteutussuunnitelmaId!: number;
+
+  @Prop({ required: true })
+  protected toteutussuunnitelmaStore!: ToteutussuunnitelmaStore;
+
+  private editointiStore: EditointiStore | null = null;
+
+  mounted() {
+    this.editointiStore = new EditointiStore(
+      new RakenneStore(
+        this.toteutussuunnitelmaId,
+        this.koulutustoimijaId,
+        async () => this.toteutussuunnitelmaStore.initNavigation(this.koulutustoimijaId, this.toteutussuunnitelmaId),
+      ));
+  }
 }
 </script>
 
