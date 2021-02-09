1bd1e56447594629a83ba96fc29b2ac2a33f8fe1
Olli-Pekka Lehtokallio <olli-pekka.lehtokallio@gofore.com>
2021-02-09 14:54:54 +0200

    EP-2639

--- src/stores/OpetussuunnitelmaPohjatStore.ts
+++ src/stores/OpetussuunnitelmaPohjatStore.ts
@@ -1,47 +1,25 @@
 import Vue from 'vue';
 import VueCompositionApi, { reactive, computed } from '@vue/composition-api';
-import { PageOpetussuunnitelmaDto, Opetussuunnitelmat, OpetussuunnitelmaDto } from '@shared/api/amosaa';
+import { PageOpetussuunnitelmaDto, Opetussuunnitelmat, OpetussuunnitelmaDto, OpetussuunnitelmaBaseDto } from '@shared/api/amosaa';
 import _ from 'lodash';
-import { IToteutussuunnitelmaProvider } from '@/components/EpToteutussuunnitelmaListaus/types';
 import { EperusteetKoulutustyyppiRyhmat } from '@shared/utils/perusteet';
 import { Toteutus } from '@/utils/toteutustypes';
-import { Page } from '@shared/tyypit';
 
 Vue.use(VueCompositionApi);
 
 export class OpetussuunnitelmaPohjatStore {
   private state = reactive({
-    opspohjat: null as OpetussuunnitelmaDto[] | null,
+    opetussuunnitelmat: null as OpetussuunnitelmaBaseDto[] | null,
   })
 
-  public readonly opspohjat = computed(() => this.state.opspohjat);
+  public readonly opetussuunnitelmat = computed(() => this.state.opetussuunnitelmat);
 
-  public async fetch(koulutustoimijaId: number, ophKtId: number, toteutus: Toteutus) {
-    const koulutustoimijanPohjat = (await Opetussuunnitelmat.getAllOpetussuunnitelmatSivutettu(
-      _.toString(koulutustoimijaId), undefined,
-      {
-        params: {
-          sivukoko: 1000,
-          tila: ['luonnos', 'valmis', 'julkaistu'],
-          tyyppi: ['opspohja'],
-          koulutustyyppi: EperusteetKoulutustyyppiRyhmat[toteutus],
-        },
-      })).data as Page<OpetussuunnitelmaDto>;
-
-    const opetushallituksenPohjat = (await Opetussuunnitelmat.getAllOpetussuunnitelmatSivutettu(
-      _.toString(ophKtId), undefined,
-      {
-        params: {
-          sivukoko: 1000,
-          tila: ['valmis', 'julkaistu'],
-          tyyppi: ['opspohja'],
-          koulutustyyppi: EperusteetKoulutustyyppiRyhmat[toteutus],
-        },
-      })).data as Page<OpetussuunnitelmaDto>;
-
-    this.state.opspohjat = [
-      ...koulutustoimijanPohjat.data,
-      ...opetushallituksenPohjat.data,
-    ];
+  public async fetch(koulutustoimijaId: number, toteutus: Toteutus, tilat: string[], tyyppi: string) {
+    this.state.opetussuunnitelmat = (await Opetussuunnitelmat.getOpsPohjat(
+      EperusteetKoulutustyyppiRyhmat[toteutus],
+      tilat,
+      tyyppi,
+      _.toString(koulutustoimijaId),
+    )).data;
   }
 }
--- src/stores/OphPohjatStore.ts
+++ src/stores/OphPohjatStore.ts
@@ -10,14 +10,17 @@ Vue.use(VueCompositionApi);
 export class OphPohjatStore implements IToteutussuunnitelmaProvider {
   private state = reactive({
     pohjat: null as OpetussuunnitelmaBaseDto[] | null,
+    opsPohjat: null as OpetussuunnitelmaBaseDto[] | null,
     opetussuunnitelmat: null as PageOpetussuunnitelmaDto | null,
   })
 
   public readonly pohjat = computed(() => this.state.pohjat);
   public readonly opetussuunnitelmat = computed(() => this.state.opetussuunnitelmat);
+  public readonly opsPohjat = computed(() => this.state.opsPohjat);
 
-  public async fetch() {
+  public async fetch(koulutustyypit: string[]) {
     this.state.pohjat = (await Opetussuunnitelmat.getPohjat()).data;
+    this.state.opsPohjat = (await Opetussuunnitelmat.getOphOpsPohjat(koulutustyypit)).data;
   }
 
   public async updateQuery(koulutustoimijaId: number, toteutus: Toteutus, query: any) {
